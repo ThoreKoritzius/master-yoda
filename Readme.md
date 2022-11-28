a75ffd6915c1b4231a5687e0447a088566b193d7
Thore Koritzius <thorekoritzius@outlook.de>
2022-11-28 15:49:53 +0100

    first commit

---
+++ Readme.md
@@ -0,0 +1,16 @@
+# Master Yoda
+--
+Natural Language Processing (NLP) is an interesting field and I wanted to experiment a bit with it. By using Spacy and POS-tagging one can build a dependency tree of a given sentence.
+This allows for the fun idea of re-arranging words in the way Master Yoda would say them:
+
+Examples:
+>"The universe is big" -> "big The universe   is"
+
+>"I love good coding" -> "good coding  I  love"
+
+>"You are the best" - > "the best  You  are"
+
+Usage
+--
+pip install spacy
+call the speak method with your sentence and just execute the python file
---
+++ master_yoda.py
@@ -0,0 +1,42 @@
+#Master Yoda: A model that restructues your sentence to speek like yoda
+#Prerequisites: pip install spacy
+#Current configuration only supports english sentences, change the spacy_model variable to switch to different model
+
+import spacy
+import os
+
+model = "en_core_web_sm"
+try:
+    nlp = spacy.load(model)
+except Exception as e:
+    #download model in case it doesnt exist
+    os.system(f'python -m spacy download {model}')
+    nlp = spacy.load(model)
+
+def get_word_graph(node):
+    if node.n_lefts + node.n_rights > 0:
+        right = [get_word_graph(child) for child in node.children]
+        return  [[node.orth_], right]
+    else:
+        return node.orth_
+
+def traverse(inp):
+    if not any(isinstance(el, list) for el in inp):
+        if isinstance(inp, str):
+            return inp
+        return ' '.join(inp)
+    res=''
+    for i in range(len(inp)):
+        res+=traverse(inp[len(inp)-1-i]) + " "
+    return res
+        
+def mr_yoda(sentence):
+    doc = nlp(sentence)
+    return traverse([get_word_graph(sent.root) for sent in doc.sents][0])
+
+def speak(sentence):
+    print("in:  " + sentence + "\nout: " + mr_yoda(sentence) + "\n")
+
+speak("The universe is big")
+speak("I love good coding")
+speak("You are the best")
